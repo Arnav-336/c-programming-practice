95801ad57ceaf2cbd70a20d7231b0318206a46eb
Arnav-336 <sahooarnav336@gmail.com>
2026-08-24 22:35:13 +0530

    Update adding_two_numbers.c

--- adding_two_numbers.c
+++ adding_two_numbers.c
@@ -2,8 +2,10 @@
 int main()
 {
     int no1,no2,sum;
-    no1=10;
-    no2=20;
+    printf("Enter 1st number: ");
+    scanf("%d",&no1);
+    printf("Enter 2nd number: ");
+    scanf("%d",&no2);
     sum=no1+no2;
     printf("The sum of %d and %d is %d",no1,no2,sum);
 }
