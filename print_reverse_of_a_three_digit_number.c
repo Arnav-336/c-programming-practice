9270358f812351a889b6bab6ccc60e10fe33de45
Arnav-336 <sahooarnav336@gmail.com>
2026-08-24 23:27:04 +0530

    Update print_reverse_of_a_three_digit_number.c

--- print_reverse_of_a_three_digit_number.c
+++ print_reverse_of_a_three_digit_number.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    int no=678;
+    int no,num;
+    printf("Enter a three digit number: ");
+    scanf("%d",&no);
+    num=no;
     int no1,no2,no3,rev;
     no1=no%10*100;
     no=no/10;
@@ -9,5 +12,5 @@ int main()
     no=no/10;
     no3=no%10*1;
     rev=no1+no2+no3;
-    printf("The reverse of 678 is %d",rev);
+    printf("The reverse of %d is %d",num,rev);
 }
