b1f93137d829140ae8b1d242e56098ebffd532c8
Arnav-336 <sahooarnav336@gmail.com>
2026-08-24 23:27:15 +0530

    Update seconds_to_hour_and_minutes.c

--- seconds_to_hour_and_minutes.c
+++ seconds_to_hour_and_minutes.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    int sec=7468;
+    int sec,num;
+    printf("Enter the time in sec: ");
+    scanf("%d",&sec);
+    num=sec;
     int hour,minute;
     hour=sec/(60*60);
     minute=(sec-(hour*60*60))%60;
-    printf("7468 seconds can also be written as %d hours and %d minutes",hour,minute);
+    printf("%d seconds can also be written as %d hours and %d minutes",num,hour,minute);
 }
