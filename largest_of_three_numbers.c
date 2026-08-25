a36f319661ce2bfa69afd388121e56d4ca8630dc
Arnav-336 <sahooarnav336@gmail.com>
2026-08-25 22:35:59 +0530

    Update largest_of_three_numbers.c

--- largest_of_three_numbers.c
+++ largest_of_three_numbers.c
@@ -8,9 +8,9 @@ int main()
     scanf("%d",&no2);
     printf("Enter the third number: ");
     scanf("%d",&no3);
-    if (no1>no2&&no1>no3)
+    if (no1>=no2&&no1>=no3)
     printf("The largest no. is %d",no1);
-    else if (no2>no3&&no2>no1)
+    else if (no2>=no3&&no2>=no1)
     printf("The largest no. is %d",no2);
     else
     printf("The largest no. is %d",no3);
