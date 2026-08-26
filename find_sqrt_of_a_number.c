d1004c49c0e5a6238c110928bd29d2fb50db8f25
Arnav-336 <sahooarnav336@gmail.com>
2026-08-26 21:14:33 +0530

    Create find_sqrt_of_a_number.c

---
+++ find_sqrt_of_a_number.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int no1,root;
+    printf("Enter the value: ");
+    scanf("%d",&no1);
+    root=sqrt(no1);
+    printf("The square root of %d is %d",no1,root);
+}
