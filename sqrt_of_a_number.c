9c11823abf599cfee7087dd9159aa885baadf6ff
Arnav-336 <sahooarnav336@gmail.com>
2026-08-27 23:36:13 +0530

    Create sqrt_of_a_number.c

---
+++ sqrt_of_a_number.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int no,root;
+    printf("Enter the number to know the square root of: ");
+    scanf("%d",&no);
+    root=sqrt(no);
+    printf("The square root of %d is %d",no,root);
+}
