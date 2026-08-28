1b12714b73431e1f44ca8291a344113f99b49e4a
Arnav-336 <sahooarnav336@gmail.com>
2026-08-28 23:16:31 +0530

    Update root_of_quadratic_equation.c

--- root_of_quadratic_equation.c
+++ root_of_quadratic_equation.c
@@ -5,7 +5,7 @@ int main()
     int a,b,c,root1,root2;
     printf("Enter the values of a, b and c of a quadratic equation: ");
     scanf("%d%d%d",&a,&b,&c);
-    root1=(-b+(sqrt((b*b)-4*a*c)/2*a));
-    root2=(-b-(sqrt((b*b)-4*a*c)/2*a));
+    root1=((-b+(sqrt((b*b)-4*a*c)))/2*a);
+    root2=((-b-(sqrt((b*b)-4*a*c)))/2*a);
     printf("\nThe roots of quadratic equation having a=%d, b=%d and c=%d are %d and %d",a,b,c,root1,root2);
 }
