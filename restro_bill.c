33f56eb75733d63eb3caf6567b580a2dcf11c1fa
Arnav-336 <sahooarnav336@gmail.com>
2026-09-09 14:00:25 +0530

    Update restro_bill.c

--- restro_bill.c
+++ restro_bill.c
@@ -8,6 +8,6 @@ int main()
     disc=(5*total)/100;
     bill1=total+gst-disc;
     bill2=total+gst;
-    final=(1000<bill1)?bill1:bill2;
+    final=(1000<total)?bill1:bill2;
     printf("Your total bill is %d",final);
 }
